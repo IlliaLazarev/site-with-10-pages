d06df98e7f2950be954659b99da81e3e5088ec6f
IlliaLazarev <lazarevillia2008@gmail.com>
2025-11-15 20:52:14 +0200

    Add changes

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// Статика з папки public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'data.json')));
 
-// Для головної сторінки
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// PORT для Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Сервер працює на порті ${PORT}`);
